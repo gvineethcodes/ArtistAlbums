f07a3523929f373e619c904fc4ada132df0a8293
gvineethcodes <gvineethfirebase@gmail.com>
2021-10-08 21:30:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/artistalbums/ParentModel.java
@@ -0,0 +1,17 @@
+package com.example.artistalbums;
+
+public class ParentModel {
+    private String artist;
+
+    public ParentModel(){}
+
+    public ParentModel(String artist) {
+        this.artist=artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+
+}
